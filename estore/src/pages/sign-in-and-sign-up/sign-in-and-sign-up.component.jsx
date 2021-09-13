4749580336f4b1f3b9cd182c32307a39cbf07766
VanVSJobs <tranquocvan2402@gmail.com>
2021-09-13 02:42:14 -0600

    sign up and sign in

--- estore/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ estore/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -4,9 +4,12 @@ import './sign-in-and-sign-up.styles.scss';
 
 import SignIn from '../../components/sign-in/sign-in.component';
 
+import Signup from '../../components/sign-up/sign-up.component';
+
 const SignInAndSignUp = () => (
-  <div>
+  <div className='sign-in-and-sign-up'>
     <SignIn />
+    <Signup />
   </div>
 );
 
