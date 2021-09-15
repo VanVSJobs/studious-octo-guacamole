bdf0ee1455c44c53be3235ea5ee3c05429c35391
VanVSJobs <tranquocvan2402@gmail.com>
2021-09-15 02:07:03 -0600

    added fancy cart and stuff

--- estore/src/components/header/header.component.jsx
+++ estore/src/components/header/header.component.jsx
@@ -10,7 +10,11 @@ import { auth } from '../../firebase/firebase.utils';
 
 import { connect } from 'react-redux';
 
-const Header = ({ currentUser }) => (
+import CartIcon from '../cart-icon/cart-icon.component';
+
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
+
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -31,12 +35,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropDown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
